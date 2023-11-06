91d22be1756ca8be840403ee722e2a039a51fac2
Rinkesh Kumar <38814803+RinkeshCharlie@users.noreply.github.com>
2023-11-06 17:55:06 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 897272;
+let accountEmail = "rinkesh.on@gmail.com";
+var accountPsw = "9988";
+accountCity = "Rafiganj";
+let accountState;
+
+// accountId = 20;
+// console.log(accountEmail);
+
+accountEmail = "data@gmail.com";
+accountPsw = "123123";
+accountCity = "Noida";
+
+console.table([accountId, accountEmail, accountPsw, accountCity, accountState]);
